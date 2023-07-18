a7eaff4fe41e2e095e0e458c19675a3be2720d80
Aishat Akinyemi <akinyemiomolola99@gmail.com>
2023-07-18 09:24:18 +0100

    Bug fix

--- redux/store.js
+++ redux/store.js
@@ -1,38 +1,18 @@
-import { configureStore, compose, applyMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import projectReducer from "./projectSlice";
-import { authIsReady, firebaseReducer } from "react-redux-firebase";
-import { firestoreReducer } from "redux-firestore";
-import thunk from "redux-thunk";
-// import { getFirebase, reactReduxFirebase } from "react-redux-firebase";
-// import { getFirestore, reduxFirestore } from "redux-firestore";
-import { app, firebaseConfig } from "@config/firebaseConfig ";
 
 export const store = configureStore({
   reducer: {
     project: projectReducer,
-    // firebase: firebaseReducer,
-    // firestore: firestoreReducer,
+   
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
 
-      // thunk: {
-      //   extraArgument: { getFirebase, getFirestore },
-      // },
     }),
 
-  // middleware: compose(
-  //   [
-  //     applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
-  //     reduxFirestore(app),
-  //     reactReduxFirebase(app, {
-  //       useFirestoreForProfile: true,
-  //       userProfile: "users",
-  //       attachAuthIsReady: true,
-  //     }),
-  //   ]
-  // ),
+
 });
 
 //  (store.getState());
